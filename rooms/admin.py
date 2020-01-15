bec8746ec0f77e283559af5f585260ffbbde571b
JeonHG <jeonhgfm101@gmail.com>
2020-01-15 18:00:36 +0900

    6.2 custom admin functions

--- rooms/admin.py
+++ rooms/admin.py
@@ -47,7 +47,9 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
+    ordering = ("name", "price", "bedrooms")
     list_filter = (
         "instant_book",
         "host__superhost",
@@ -60,3 +62,9 @@ class RoomAdmin(admin.ModelAdmin):
     )
     filter_horizontal = ("amenities", "facilities", "house_rules")
     search_fields = ("^city", "host__username")
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "Hello"
